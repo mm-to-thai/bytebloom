3573698a295c7391353f63d8bb1c0741744702ae
AungBoMyint <aungbomyint292@gmail.com>
2024-01-13 21:38:06 +0630

    change cors

--- root/settings.py
+++ root/settings.py
@@ -69,23 +69,8 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
+    'http://127.0.0.1:8000',
 ]
-CORS_ALLOW_METHODS = [
-    'GET',
-]
-
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
-CORS_URLS_REGEX = r'^/media/.*$'
 
 ROOT_URLCONF = 'root.urls'
 
