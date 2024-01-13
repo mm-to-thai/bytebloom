ac5bb5e86a154204e60d8211dfece13fb6ea3734
AungBoMyint <aungbomyint292@gmail.com>
2024-01-13 21:35:32 +0630

    fix cors

--- root/settings.py
+++ root/settings.py
@@ -70,7 +70,21 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
 ]
+CORS_ALLOW_METHODS = [
+    'GET',
+]
 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 CORS_URLS_REGEX = r'^/media/.*$'
 
 ROOT_URLCONF = 'root.urls'
